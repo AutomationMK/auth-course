76929dd0ab93b62611748f94a34ba9ed033da36f
Max Kranker <maxkranker@protonmail.com>
2026-05-07 03:18:55 -0400

    Add rotating key struct along with generateNewKey helper function

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/AutomationMK/auth-course
 go 1.26.2
 
 require (
+	github.com/gofrs/uuid/v5 v5.4.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	golang.org/x/crypto v0.50.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha512"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
+	"github.com/gofrs/uuid/v5"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -22,13 +26,7 @@ func (u *UserClaims) Validate() error {
 	return nil
 }
 
-var key = []byte{}
-
 func main() {
-	for i := 1; i <= 64; i++ {
-		key = append(key, byte(i))
-	}
-
 	pass := "123456789"
 
 	hashedPass, err := hashPassword(pass)
@@ -61,7 +59,7 @@ func comparePassword(password string, hashedPass []byte) error {
 }
 
 func signMessage(msg []byte) ([]byte, error) {
-	h := hmac.New(sha512.New, key)
+	h := hmac.New(sha512.New, keys[currentKid].key)
 	_, err := h.Write(msg)
 	if err != nil {
 		return nil, fmt.Errorf("Error in signMessage while hashing message: %w", err)
@@ -83,12 +81,39 @@ func checkSig(msg, sig []byte) (bool, error) {
 
 func createToken(c *UserClaims) (string, error) {
 	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
-	signedToken, err := t.SignedString(key)
+	signedToken, err := t.SignedString(keys[currentKid].key)
 	if err != nil {
 		return "", fmt.Errorf("Error in createToken when signing Token: %w", err)
 	}
 	return signedToken, nil
 }
+
+func generateNewKey() error {
+	newKey := make([]byte, 64)
+	_, err := io.ReadFull(rand.Reader, newKey)
+	if err != nil {
+		return fmt.Errorf("Error in generateNewKey while generating key: %w", err)
+	}
+	uid, err := uuid.NewV7()
+	if err != nil {
+		return fmt.Errorf("Error in generateNewKey while generating kid: %w", err)
+	}
+
+	keys[uid.String()] = key{
+		key:     newKey,
+		created: time.Now(),
+	}
+	currentKid = uid.String()
+	return nil
+}
+
+type key struct {
+	key     []byte
+	created time.Time
+}
+
+var currentKid = ""
+var keys = map[string]key{}
 
 func parseToken(tokenStr string) (*UserClaims, error) {
 	t, err := jwt.ParseWithClaims(tokenStr, &UserClaims{}, func(t *jwt.Token) (any, error) {
@@ -96,7 +121,17 @@ func parseToken(tokenStr string) (*UserClaims, error) {
 			return nil, fmt.Errorf("Invalid signing algorithm")
 		}
 
-		return key, nil
+		kid, ok := t.Header["kid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("Invalid key ID")
+		}
+
+		k, ok := keys[kid]
+		if !ok {
+			return nil, fmt.Errorf("Invalid key ID")
+		}
+
+		return k, nil
 	})
 	if err != nil {
 		return nil, fmt.Errorf("Error in parseToken while parsing token: %w", err)
