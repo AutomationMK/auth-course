f818c006b31328b87abbe00aa2053df9312f507e
Max Kranker <maxkranker@protonmail.com>
2026-05-11 07:21:42 -0400

    Add initial Github oauth2 test

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gofrs/uuid/v5 v5.4.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	golang.org/x/crypto v0.50.0 // indirect
+	golang.org/x/oauth2 v0.36.0 // indirect
 )
---
+++ oauth2/oauth2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
+	ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
+	Endpoint:     github.Endpoint,
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/oauth/github", startGithubOauth)
+	http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	html := `<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title>Ninja Level 2 Exercise 6</title>
+			<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+		</head>
+		<body class="flex flex-col items-center gap-20 mx-auto container px-4 pt-10 bg-slate-50">
+			<form action="/oauth/github" method="POST"
+				class="grid justify-items-center xl:grid-cols-2 bg-blue-300 w-full max-w-6xl gap-4 p-8 rounded-md shadow-md shadow-slate-500/20">
+				<h2 class="text-xl md:text-2xl lg:text-4xl lg:col-span-full text-slate-50 font-bold text-center">
+					Login Now
+				</h2>
+				<input class="lg:col-span-full bg-yellow-100 text-slate-800 px-4 py-2 font-bold rounded-md" type="submit" value="Login with Github">
+			</form>
+		</body>
+	</html>`
+	http.ResponseWriter.Write(w, []byte(html))
+}
+
+func startGithubOauth(w http.ResponseWriter, r *http.Request) {
+	redirectURL := githubOauthConfig.AuthCodeURL("0000")
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
